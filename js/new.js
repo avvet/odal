d6dd37bacedc1dc1af0a6575ffb3306cf4d9a98f
avvet <sveta.drozdovich@yandex.ru>
2018-03-15 11:17:15 +0300

    New class static added

--- js/new.js
+++ js/new.js
@@ -1,1 +1,14 @@
 console.log('NEW JS');
+
+class User {
+  constructor(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  static getClass(){
+    return new User('vasya','petrov');
+  }
+};
+
+let user = User.getClass();
+console.log(user.firstName);
